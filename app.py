7a49c4e2f44087b8401aba08a599cb717815ffc2
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2023-12-23 15:49:38 -0500

    asdf

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
-
-
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask
- 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
-app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
- 
-# main driver function
-if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Heroku!'
+
+if __name__ == '__main__':
+    # Use the PORT environment variable if defined, otherwise default to 
+5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
